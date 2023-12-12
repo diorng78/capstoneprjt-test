63b36e0ae0037e4207c9becb4ec1e4970a546ecf
diorng78 <diorng78@yahoo.com.sg>
2023-12-12 21:54:12 +0800

    update var file

--- variable.tf
+++ variable.tf
@@ -5,5 +5,5 @@ variable "bucket-name" {
 
 # Domain name that you have registered
 variable "domain-name" {
-  default = "http://${aws_s3_bucket.my-static-website.bucket}.s3-website.us-east-1.amazonaws.com" // Modify as per your domain name
+  default = {} // Modify as per your domain name
 }
